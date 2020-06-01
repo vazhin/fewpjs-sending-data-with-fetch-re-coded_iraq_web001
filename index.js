fd668b43c0cccd3932fc2390d64ac901165ab514
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-06-01 13:10:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+
+function submitData (userName, userEmail) {
+  const body = document.querySelector('body')
+  return fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: userEmail
+    })
+  })
+  .then((response) => response.json())
+  .then((object) => {
+    document.body.innerHTML = object['id']
+  })
+  .catch((error) => {
+    document.body.innerHTML = error.message
+  })
+}
